3612d1fb8a49308aeca32252a49a89b1eea27430
ronesta <gvozzzd123@gmail.com>
2025-04-22 19:07:18 +0300

    AlbumViewControllerSnapshotTests

--- iTunesUserDefaultsCleanSwift/iTunesTests/iTunes/AlbumScene/View /AlbumViewControllerSnapshotTests.swift
+++ iTunesUserDefaultsCleanSwift/iTunesTests/iTunes/AlbumScene/View /AlbumViewControllerSnapshotTests.swift
@@ -1,14 +1,15 @@
 //
-//  AlbumViewControllerTests.swift
+//  AlbumViewControllerSnapshotTests.swift
 //  iTunesUserDefaultsCleanSwift
 //
 //  Created by Ибрагим Габибли on 22.04.2025.
 //
 
 import XCTest
+import SnapshotTesting
 @testable import iTunesUserDefaultsCleanSwift
 
-final class AlbumViewControllerTests: XCTestCase {
+final class AlbumViewControllerSnapshotTests: XCTestCase {
     private var mockInteractor: MockAlbumInteractor!
     private var viewController: AlbumViewController!
 
@@ -24,14 +25,8 @@ final class AlbumViewControllerTests: XCTestCase {
         super.tearDown()
     }
 
-    func testViewDidLoadCallsInteractor() {
-        viewController.viewDidLoad()
-
-        XCTAssertTrue(mockInteractor.viewDidLoadCalled)
-    }
-
-    func testDisplayAlbumDetailsUpdatesUI() {
-        let image = UIImage()
+    func testAppearanceWithData() {
+        let image = UIImage(systemName: "checkmark.diamond")
         let album = Album(artistId: 111051,
                           artistName: "Eminem",
                           collectionName: "The Eminem Show",
@@ -39,13 +34,10 @@ final class AlbumViewControllerTests: XCTestCase {
                           collectionPrice: 10.99
                          )
 
-        let viewModel = AlbumModels.ViewModel(album: album, image: image)
+        let viewModel = AlbumModels.ViewModel(album: album, image: image ?? UIImage())
 
         viewController.displayAlbumDetails(viewModel: viewModel)
 
-        XCTAssertEqual(viewController.albumNameLabel.text, "The Eminem Show")
-        XCTAssertEqual(viewController.artistNameLabel.text, "Eminem")
-        XCTAssertEqual(viewController.collectionPriceLabel.text, "10.99 $")
-        XCTAssertEqual(viewController.albumImageView.image, image)
+        assertSnapshot(of: viewController, as: .image)
     }
 }
